2658df0c709cd22723d30c32caa37bebbe6a58ae
Osakpolor Noyiagbon <osak830@gmail.com>
2022-03-21 15:59:18 -0400

    Python version done!

---
+++ cake.py
@@ -0,0 +1,8 @@
+cakeorbust = input("Do you want cake? (yes/no) ")
+
+if cakeorbust == "yes":
+    print("Have some cake")
+elif cakeorbust == "no":
+    print("No cake for you.")
+else:
+    print("Your response is other than what I asked for.")
